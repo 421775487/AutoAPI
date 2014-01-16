cf80a855063b71e489727c697b1b63da15deb0b9
heulizeyang <heulizeyang@gmail.com>
2014-01-16 17:15:16 +0800

    修改lib下面函数库文件名，优化部分代码

--- config/sysconfig.py
+++ config/sysconfig.py
@@ -1,36 +1,31 @@
 # -*-coding:utf-8 -*-
-# author:zeyang@staff.sina.com.cn
-# time:2013-12-12
-# Version:1.0
-# config.py:save the testing needed information
+# author: zeyang@staff.sina.com.cn
+# time:   2013-12-12
 
 import os
 _WORKSPACE = os.getcwd()[:-7]
 
-uname = "yangzl296017245"
-pwd = 13397276388
-
 # test app information.
-source = "4125898983"
-access_token = "2.00mG98WBnnqNVEfe7acc1871Kgp6XC"
+_SOURCE = "4125898983"
+_ACCESS_TOKEN = "2.00mG98WBnnqNVEfe7acc1871Kgp6XC"
 
 # api reference information
 apiRef = {
-'STATUS':\
+'status':\
 	{'apis':['queryid','querymid','show'],\
 	'name':'微博组接口',\
 	'path':_WORKSPACE + '/data/api/status'},
-'OPEN':\
-	{'apis':[],\
-	'name':'平台组接口',\
+'open':\
+	{'apis':[],
+	'name':'平台组接口',
 	'path':_WORKSPACE + '/data/api/open'},
-'OAUTH':\
+'oauth':\
 	{'apis':[],\
 	'name':'授权组接口',\
 	'path':_WORKSPACE + '/data/api/oauth'}
 }
 
-# path
-PLANPATH = _WORKSPACE + '/plan'
+# work dict path
+_PLANPATH = _WORKSPACE + '/plan'
 _APIPATH = _WORKSPACE + '/data/api'
 _CASEPATH = _WORKSPACE + '/data/case'
